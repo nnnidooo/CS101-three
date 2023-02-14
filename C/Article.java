d613bebfaa78fd4721e16f60c8f8ebf1145bbce4
nnnidooo <nadi3252@stud.kea.dk>
2023-02-14 08:29:48 +0100

    C igang

---
+++ C/Article.java
@@ -0,0 +1,35 @@
+package C;
+
+import java.util.ArrayList;
+
+public class Article {
+
+    private String author;
+    private String title;
+
+
+    public Article(String author, String title) {
+        this.author=author;
+        this.title=title;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Article> articles = new ArrayList<>();
+        articles.add(new Article("Chris Jakobsen","Hvorfor får man SU i DK?"));
+        articles.add(new Article("Helle Nielsen","Sådan får du en naturlig glød"));
+        articles.add(new Article("Tom Frydegaard","Hvorfor fuldtids-arbejde nedslider en"));
+        articles.add(new Article("Katrine Hansen","Hvorfor vi skal have flere almenboliger"));
+        articles.add(new Article("Dean Morgan","Why you should invest in crypto"));
+
+        System.out.println(articles);
+    }
+}
---
+++ D/RedditPost.java
@@ -0,0 +1,25 @@
+package D;
+
+public class RedditPost {
+    private Double Date;
+    private String author;
+    private int balance;
+    private String title;
+
+    public Double getDate() {
+        return Date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
